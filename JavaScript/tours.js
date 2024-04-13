26f21dee7c1b8d51aafdc5440a8cb87b8adc1b83
Valentin1512 <valentinviollaz78@gmail.com>
2024-04-12 22:34:31 -0300

    añadi archivos de javascript

---
+++ JavaScript/tours.js
@@ -0,0 +1,4 @@
+let nombre = prompt("Cual es tu nombre?")
+let edad = parseInt(prompt("Cual es tu edad?"))
+
+alert("Hola " + nombre + " de " + edad + " años " + " te interesaría adquirir tickects ? 🎟️'")
